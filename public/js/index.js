110036af5ad99dc1769b7e3d1737ae09f245342b
JinHeeDo-prog <114121285+JinHeeDo-prog@users.noreply.github.com>
2022-12-08 11:54:25 +0900

    클릭 포인터 변경// 코드 간략화

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,12 @@
 const headUserNameElement = document.querySelector('.user');
-const userNameElement = document.querySelector('.user-name');
+const userNameElement = document.querySelector('#name');
+
 const localUserName = localStorage.getItem('userName');
+
 
 const setUserNameInnerHtml = (name) => {
   headUserNameElement.innerHTML = '${name} <span>님</span>';
-  userNameElement.innerHTML = '${name} <span>님</span>';
+  userNameElement.textContent = name;
 };
 
 
@@ -12,13 +14,10 @@ if (localUserName) {
   setUserNameInnerHtml(localUserName);
 }
 
-
 userNameElement.onclick = () => {
   const userName = prompt('이름을 입력해 주세요.');
 
   localStorage.setItem('userName', userName);
-
-  headUserNameElement.innerHTML = '${userName} <span>님</span>';
-  userNameElement.innerHTML = '${userName} <span>님</span>';
+  setUserNameInnerHtml(userName);
 };
 
